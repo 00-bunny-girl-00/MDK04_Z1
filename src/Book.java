82d391ab6d2e4ba6d228119f66aa61189c7260d2
00-bunny-girl-00 <vesele505@gmail.com>
2023-02-06 21:29:54 +0300

    МДК_04

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book implements Printable{
+    private final String s;
+
+    public Book(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void print() {
+        System.out.println(getS());
+    }
+    static void printBooks(Printable[] printable){
+        for (Printable q: printable){
+            if (q instanceof Book){
+                q.print();
+            }
+        }
+    }
+}
